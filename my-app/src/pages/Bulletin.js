3d10f9554c91631265e5e959515f9799d2b1ca13
Kent Valle <kentvalle1@gmail.com>
2020-05-14 19:43:38 -0500

    dont mind the my-app file, just pay attention to the new-app

--- my-app/src/pages/Bulletin.js
+++ my-app/src/pages/Bulletin.js
@@ -14,6 +14,7 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Modal from "@material-ui/core/Modal";
 import SnackBar from "../components/SnackBar/SnackBar";
+import ReactMapGL from "react-map-gl";
 
 function getModalStyle() {
   const top = 50;
